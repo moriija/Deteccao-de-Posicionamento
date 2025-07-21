76316030db991066798c5cba5a72d30bc591af94
Isa <isa.cmorija@gmail.com>
2025-07-21 20:01:49 -0300

    embeddings geradas

--- pre-processamento.py
+++ pre-processamento.py
@@ -1,10 +1,22 @@
 # %%
 import pandas as pd
-import networkx as nx
-df_ori = pd.read_csv('Dados/aborto-consolidated-parent-based_treinamento.tsv', sep='\t', decimal = ',', encoding = 'UTF-8')
-print(df_ori.columns)
+import numpy as np
+# import networkx as nx
+
+# %% 
+
+# https://huggingface.co/neuralmind/bert-base-portuguese-cased
+# BERTimbau 
+from transformers import AutoTokenizer  # Or BertTokenizer
+from transformers import AutoModelForPreTraining  # Or BertForPreTraining for loading pretraining heads
+from transformers import AutoModel  # or BertModel, for BERT without pretraining heads
+
+import os
+import torch
 
 # %%
+df_ori = pd.read_csv('Dados/aborto-consolidated-parent-based_treinamento.tsv', sep='\t', decimal = ',', encoding = 'UTF-8')
+print(df_ori.columns)
 
 # MANIPULAÇÃO DO DF
 
@@ -14,7 +26,6 @@ df = df.drop(columns=['parent_message', 'target_and_message', 'target_parent_mes
 # deixaremos parent_label por enquanto
 # target_id é a chave da thread dos comentarios com o mesmo Alvo.
 
-# %%
 # definir a label do comentário original como "Comentário Original". (Apenas metade das linhas tinham)
 df.loc[df['id'] == '1', 'label'] = 'Comentário Original'
 
@@ -24,3 +35,45 @@ df.loc[df['parent_id'] == '1', 'parent_label'] = 'Comentário Original'
 # podemos considerar "Comentário Original" posteriormente como "Concorda".
 
 # %%
+#Importando o Modelo BERTimbau
+model = AutoModel.from_pretrained("neuralmind/bert-base-portuguese-cased")
+tokenizer = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+
+
+# %%
+# Geração das Embeddings
+def getEmbeddings(text, tokenizer, model):
+
+    inputs = tokenizer(
+        text, 
+        return_tensors="pt",      
+        truncation=True,          # corta textos longos para o tamanho máximo
+        padding=True,             # adiciona padding para igualar o tamanho dos textos
+        max_length=512            # tamanho máximo suportado pelo BERT
+    )
+
+    # função do torch que não utiliza gradientes (vamos apenas extrair embeddings)
+    with torch.no_grad():
+        outputs = model(**inputs)  # passa os tokens pelo modelo BERT
+    # Pega o embedding do token [CLS] (primeiro token), que representa o texto inteiro
+    return outputs.last_hidden_state[:, 0, :].squeeze().numpy()
+
+# %%
+# Gerar embeddings das mensagens de cada comentário
+embeddings = [getEmbeddings(text, tokenizer, model) for text in df['message']]
+
+# %%
+df['embedding'] = embeddings
+
+# checar por duplicatas
+df['embedding_tuple'] = df['embedding'].apply(lambda x: tuple(x)) # Converter cada embedding para tupla (ou string) para comparação
+duplicates = df[df.duplicated(subset=['embedding_tuple'], keep=False)]  # Agora sim, checar por duplicatas de conteúdo
+df = df.drop(columns=['embedding_tuple'])
+# são mensagens duplicadass irrelevantes
+
+# checar por nulos
+nulls = df[df['embedding'].isnull()] # 0
+
+# salvar o DataFrame com as embeddings (demora pra gerar )
+df.to_csv('embeddings/aborto-consolidated-parent-based_treinamento_embeddings.tsv', sep='\t', decimal = ',', encoding = 'UTF-8')
+
